384a490e461ad2ddc5752bf95fcd41f43579bf69
AbdiGureye <abdifatah.gureye@gmail.com>
2021-11-09 21:45:37 +0000

    express server

--- express_server.js
+++ express_server.js
@@ -6,6 +6,11 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+function generateRandomString() {
+return Math.random().toString(36).slice(5) 
+}
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -16,6 +21,10 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
@@ -23,6 +32,10 @@ app.get("/urls", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
+});
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
